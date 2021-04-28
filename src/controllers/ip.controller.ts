4746ed0ca3c89ba334d6e77743753aae4fc8ab11
Nick <nicholas.havens@karglobal.com>
2021-04-28 11:45:22 -0500

    chore: add error handling

--- src/controllers/ip.controller.ts
+++ src/controllers/ip.controller.ts
@@ -2,22 +2,21 @@ import { NextFunction, Request, Response } from 'express';
 import { indexedIps } from '../globals'
 
 class IpController {
-  public checkIps = async ({ body }: Request, res: Response, next: NextFunction): Promise<void> => {
-      try {
-        const blacklisted = {}
+  public checkIps = async ({ body }: Request, res: Response): Promise<void> => {
+    if (body.ipList || !body.ipList.length) {
+      res.status(400).send('At least 1 IP address must be included in body of request')
+    } else {
+      const blacklisted = {}
 
-        for (const entry of body.ipList) {
-          if (indexedIps.indexed[entry] && !blacklisted[entry]) {
-            blacklisted[entry] = indexedIps.indexed[entry]
-          }
+      for (const entry of body.ipList) {
+        if (indexedIps.indexed[entry] && !blacklisted[entry]) {
+          blacklisted[entry] = indexedIps.indexed[entry]
         }
-        res.status(200).json({ blacklisted, message: 'success' });
-
-      } catch (error) {
-        console.log("ERROR", error)
-        next(error);
       }
+
+      res.status(200).json({ blacklisted, message: 'success' });
     }
+  }
 }
 
 export default IpController;
